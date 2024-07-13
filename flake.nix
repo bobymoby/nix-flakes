88b2af72daa5ff4eab31124bdd89ae3e09df303b
Borimir Georgiev <imbobimobi@gmail.com>
2024-07-13 21:18:25 +0300

    add nixpkgs input

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Nix flake utilities and templates";
-
+    inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs = inputs: {
     lib = import ./lib;
     templates = import ./templates;
