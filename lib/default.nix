494993f65a0c7df20319db5f4ac1aef70fbe67ea
Borimir Georgiev <imbobimobi@gmail.com>
2024-07-13 21:12:19 +0300

    fixes

--- lib/default.nix
+++ lib/default.nix
@@ -1,6 +1,6 @@
 let
   systems = import ./systems.nix;
-  system-flake-tools = import ./system-flake-tools.nix;
+  system-flake-tools = import ./system-flake-tools;
   lib = {
     inherit systems system-flake-tools;
   };
